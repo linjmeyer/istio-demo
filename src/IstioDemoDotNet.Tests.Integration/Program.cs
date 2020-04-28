e4cc1ce5998fa7ca8742a2527a8106a9bc9aa786
Lin Meyer <lin.meyer@redbox.com>
2020-04-28 09:34:25 -0500

    Added --fail argument to integration tests

--- src/IstioDemoDotNet.Tests.Integration/Program.cs
+++ src/IstioDemoDotNet.Tests.Integration/Program.cs
@@ -1,17 +1,26 @@
 ﻿using System;
+using System.Linq;
 using System.Threading;
 
 namespace IstioDemoDotNet.Tests.Integration
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
             Console.WriteLine("I am a placeholder for real integration tests!");
             Console.WriteLine("Tests starting....");
             Console.WriteLine("Results: ");
+            if (args.Any(a => string.Equals("--fail", a, StringComparison.OrdinalIgnoreCase)))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("All Tests failed!");
+                return 1;
+            }
+            
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("All Tests passed!");
+            return 0;
         }
     }
 }

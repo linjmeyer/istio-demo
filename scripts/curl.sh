920c1b2b15244b39a09ef00c370e8e869a1b3399
Lin Meyer <lin.meyer@redbox.com>
2020-04-28 13:07:28 -0500

    Added istio gateway and more curl examples

--- scripts/curl.sh
+++ scripts/curl.sh
@@ -1,4 +1,15 @@
+# Inside cluster curl
 while true; do curl demo/api/demo && echo ""; done
 
+# Inside cluster curl to canary
 while true; do curl -H "x-demo-environment: canary"  demo/api/demo && echo ""; done
+
+# Outside cluster curl
+while true; do curl demo.lin/api/demo && echo ""; done
+
+# Outside cluster curl to canary
+while true; do curl demo.lin/api/demo -H "x-demo-environment: canary" && echo ""; done
+
+# Outside cluster curl to prod
+while true; do curl demo.lin/api/demo -H "x-demo-environment: prod" && echo ""; done
 

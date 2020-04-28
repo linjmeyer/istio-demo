1a0afda55523a4a792d6e51646a1d90a37b9df0c
Lin Meyer <lin.meyer@redbox.com>
2020-04-28 10:24:15 -0500

    Added http calls to tests

--- src/IstioDemoDotNet.Tests.Integration/Program.cs
+++ src/IstioDemoDotNet.Tests.Integration/Program.cs
@@ -1,26 +1,26 @@
 ﻿using System;
 using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace IstioDemoDotNet.Tests.Integration
 {
-    class Program
+    static class Program
     {
-        static int Main(string[] args)
+        static async Task<int> Main(string[] args)
         {
             Console.WriteLine("I am a placeholder for real integration tests!");
             Console.WriteLine("Tests starting....");
             Console.WriteLine("Results: ");
-            if (ShouldFail(args))
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("All Tests failed!");
-                return 1;
-            }
+
+            if (ShouldFail(args)) return Fail();
             
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("All Tests passed!");
-            return 0;
+            var tests = await DoTests();
+            if (!tests) return Fail();
+            
+            return Pass();
         }
 
         static bool ShouldFail(string[] args)
@@ -28,6 +28,46 @@ namespace IstioDemoDotNet.Tests.Integration
             return args.Any(a => string.Equals("--fail", a, StringComparison.OrdinalIgnoreCase))
                    || string.Equals(Environment.GetEnvironmentVariable("DEMO_TESTS_FAIL"), "true",
                        StringComparison.OrdinalIgnoreCase);
+        }
+
+        static int Pass()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("All Tests passed!");
+            return 0;
+        }
+        
+        static int Fail()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("All Tests failed!");
+            return 1;
+        }
+
+        static string GetUrl()
+        {
+            return Environment.GetEnvironmentVariable("DEMO_TESTS_URL");
+        }
+
+        static async Task<bool> DoTests()
+        {
+            using (var client = new HttpClient())
+            {
+                var url = GetUrl();
+                for (var i = 0; i < 25; i++)
+                {
+                    var result = await client.GetAsync(url);
+                    var resultBodyJson = await result.Content.ReadAsStringAsync();
+                    Console.WriteLine(resultBodyJson.RemoveNewlines());
+                }
+            }
+
+            return true;
+        }
+
+        static string RemoveNewlines(this string item)
+        {
+            return Regex.Replace(item, @"\t|\n|\r", string.Empty);
         }
     }
 }
